e4417ead01df25854ce109caab962641cc177202
unknown <you@example.com>
2024-09-01 11:42:55 -0300

    equipes

--- test_qrcode.py
+++ test_qrcode.py
@@ -1,7 +1,7 @@
 import qrcode
 from qrcode.constants import ERROR_CORRECT_L
 
-url = "http://equipekb.shop:8000/app/appbanco"
+url = "http://equipepriscila.shop:8080/app/appbanco"
 qr = qrcode.QRCode(
     version=1,
     error_correction=ERROR_CORRECT_L,
@@ -12,5 +12,5 @@ qr.add_data(url)
 qr.make(fit=True)
 
 img = qr.make_image(fill='black', back_color='white')
-img.save("equipekb.png")
+img.save("equipepriscila.png")
 print("QR code generated successfully!")
