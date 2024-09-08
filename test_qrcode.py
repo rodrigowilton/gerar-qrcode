99d7176a49432e51186ef8d8bcf3188def59bcb8
unknown <you@example.com>
2024-09-08 16:06:05 -0300

    novos qrcodes

--- test_qrcode.py
+++ test_qrcode.py
@@ -1,7 +1,7 @@
 import qrcode
 from qrcode.constants import ERROR_CORRECT_L
 
-url = "http://equipepriscila.shop:8080/app/appbanco"
+url = "http://equipeguilhermeleal.shop:8688/app/appbanco"
 qr = qrcode.QRCode(
     version=1,
     error_correction=ERROR_CORRECT_L,
@@ -12,5 +12,5 @@ qr.add_data(url)
 qr.make(fit=True)
 
 img = qr.make_image(fill='black', back_color='white')
-img.save("equipepriscila.png")
+img.save("equipeguilhermelelal.png")
 print("QR code generated successfully!")
