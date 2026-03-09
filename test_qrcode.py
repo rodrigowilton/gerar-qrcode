0907983417c6adbda9f49eed9d0bf6b4c1c226b8
rodrigo.muniz <rodrigo.muniz@tatticasegunca.com.br>
2026-03-09 08:00:40 -0300

    jorginho

--- test_qrcode.py
+++ test_qrcode.py
@@ -1,7 +1,7 @@
 import qrcode
 from qrcode.constants import ERROR_CORRECT_L
 
-url = "http://equipeguilhermeleal.shop:8688/app/appbanco"
+url = "http://jorginho.streetonline.com.br:3000/app/appbancouios/"
 qr = qrcode.QRCode(
     version=1,
     error_correction=ERROR_CORRECT_L,
@@ -12,5 +12,5 @@ qr.add_data(url)
 qr.make(fit=True)
 
 img = qr.make_image(fill='black', back_color='white')
-img.save("equipeguilhermelelal.png")
+img.save("equipejorginho.png")
 print("QR code generated successfully!")
